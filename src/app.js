f1d1168cbf39d71e30d88f18212465e0f91b917a
James Hurley <jhurley070@gmail.com>
2020-04-04 17:24:45 +0100

    Some style cleanup

--- src/app.js
+++ src/app.js
@@ -3,12 +3,22 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import FileTable from "./components/fileTable";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+    main: {
+        marginTop: 100,
+        marginBottom: 100,
+    },
+});
 
 export default ({ data }) => {
+    const classes = useStyles();
+
     return (
         <React.Fragment>
             <CssBaseline />
-            <Container maxWidth="sm">
+            <Container maxWidth="sm" className={classes.main}>
                 <Typography variant="h3" gutterBottom>
                     CSV App
                 </Typography>
