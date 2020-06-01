2693157f6dfed8292c5abd02a5aba6c5faa9d3ef
James Hurley <jhurley070@gmail.com>
2020-06-01 22:19:39 +0100

    Different ways to start gunicorn

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -7,3 +7,18 @@ export WEBPACK_MANIFEST=dist/manifest.json
 export WEBPACK_APP=app.js
 
 gunicorn --bind 0.0.0.0:8000 wsgi
+
+# gunicorn -b 0.0.0.0:8080 -w 3 wsgi
+# gunicorn --daemon --workers 3 --bind unix:/home/vagrant/csvapp.sock wsgi
+# gunicorn --bind unix:/home/vagrant/csvapp.sock wsgi
+
+# [program:csvapp]
+# command=/home/vagrant/pyenv/shims/gunicorn --workers 3 --bind unix:/home/vagrant/csvapp.sock wsgi
+# directory=/opt/projects/CSVApp
+# autostart=true
+# autorestart=true
+# stderr_logfile=/var/log/csvapp.err.log
+# stdout_logfile=/var/log/csvapp.out.log
+
+# [supervisord]
+# environment=FLASK_APP=csvapp/__init__.py,FLASK_ENV=production,UPLOAD_FOLDER=uploads/csv,WEBPACK_MANIFEST=dist/manifest.json,WEBPACK_APP=app.js
